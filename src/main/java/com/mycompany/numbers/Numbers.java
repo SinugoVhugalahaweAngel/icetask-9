03ee4d3203b423fb8928b057c8bd0a04a8e6ee7a
Angel Vhugie <Angel Vhugie@DESKTOP-T6KBT5Q>
2024-10-10 20:13:48 +0200

    ..

---
+++ src/main/java/com/mycompany/numbers/Numbers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.numbers;
+
+/**
+ *
+ * @author Angel Vhugie
+ */
+public class Numbers {
+
+    public static void main(String[] args) {
+        
+    }
+}
