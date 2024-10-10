37c557d9c5d0b6a94e994c3085302031c722c614
Angel Vhugie <Angel Vhugie@DESKTOP-T6KBT5Q>
2024-10-10 20:21:36 +0200

    Prompt the user to enter first number and second number

--- src/main/java/com/mycompany/numbers/Numbers.java
+++ src/main/java/com/mycompany/numbers/Numbers.java
@@ -11,6 +11,20 @@ package com.mycompany.numbers;
 public class Numbers {
 
     public static void main(String[] args) {
+        int num1,num2,results=0;
+        //Creatingf object for a scanner
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Enter First number");
+        num1= sc.nextInt();
+        
+        System.out.println("Enter Second number");
+        num2= sc.nextInt();
+        
+        System.out.print("====Menu=====");
+        System.out.println("Enter the corresponding number of item in the menu");
+        
+       
         
     }
 }
